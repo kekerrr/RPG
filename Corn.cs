0fe76cf98bae6e9f8d50dbd50770f1010367b81b
kekerrr <158700520+kekerrr@users.noreply.github.com>
2024-06-25 21:48:57 +0500

    Add files via upload
    настроили анимацию врагу  
    реализовали нанесение урона кукурузе
    добавили маску урона врагам

--- Corn.cs
+++ Corn.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Corn : MonoBehaviour
 {
     public static Corn singleton;
-    
+    public int hp = 100;
+
     private void Awake()
     {
         singleton = this;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            print("кукуруза меньше 0 хп");
+        }
     }
 }
--- Enemy.cs
+++ Enemy.cs
@@ -7,12 +7,30 @@ public class Enemy : MonoBehaviour
     public int health = 10;
     public int speed = 1;
     public float borderPosX;
+    public Animator animator;
+    public int damage = 5;
+    public float hitInterval = 1;
+    public float hitTimer = 0;
 
     void Update()
     {
         if (gameObject.transform.position.x >= borderPosX)
         {
             transform.position += -transform.right * speed * Time.deltaTime;
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+            if (hitTimer <= 0)
+            {
+                DealDamage();
+                hitTimer = hitInterval;
+            }
+            else
+            {
+                hitTimer -= Time.deltaTime;
+            }
         }
     }
 
@@ -29,4 +47,10 @@ public class Enemy : MonoBehaviour
     {
         borderPosX = Corn.singleton.transform.position.x;
     }
+
+    public void DealDamage()
+    {
+        Corn.singleton.TakeDamage(damage);
+    }
+
 }
